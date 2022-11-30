9e1053099c5a0ad277112440758d0b2805730f78
45marcin <45marcin@gmail.com>
2022-11-30 14:00:22 +0100

    cam 1 pins added to leds

---
+++ generate_kernel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+
+
+sudo MACHINE=imx8mm-var-dart ./var_make_debian.sh -c kernel
+sudo MACHINE=imx8mm-var-dart ./var_make_debian.sh -c modules
+sudo cp output/Image.gz /media/marcin/rootfs/boot/Image.gz
+sudo cp output/imx8mm-var-dart-dt8mcustomboard-legacy.dtb /media/marcin/rootfs/boot/imx8mm-var-dart-dt8mcustomboard-legacy.dtb
+sudo sync
