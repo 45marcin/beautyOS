8fb7b0afb56fc308f9b8350954d1bd90cb78bc6a
45marcin <45marcin@gmail.com>
2022-12-01 12:41:02 +0100

    cam_1_works

--- generate_kernel.sh
+++ generate_kernel.sh
@@ -1,9 +1,12 @@
 #!/bin/bash 
 
 
-
+echo "Generating kernel & modules"
 sudo MACHINE=imx8mm-var-dart ./var_make_debian.sh -c kernel
 sudo MACHINE=imx8mm-var-dart ./var_make_debian.sh -c modules
 sudo cp output/Image.gz /media/marcin/rootfs/boot/Image.gz
 sudo cp output/imx8mm-var-dart-dt8mcustomboard-legacy.dtb /media/marcin/rootfs/boot/imx8mm-var-dart-dt8mcustomboard-legacy.dtb
 sudo sync
+sudo umount /media/marcin/rootfs 
+
+echo "Sdcard ready for device"
