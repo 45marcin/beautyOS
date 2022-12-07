7b5ebfc5925195a089fc9013a2ceeca54f897b08
RosiekAmctech <107031521+RosiekAmctech@users.noreply.github.com>
2022-12-07 19:36:18 +0100

    impr

--- generate_kernel.sh
+++ generate_kernel.sh
@@ -4,9 +4,9 @@
 echo "Generating kernel & modules"
 sudo MACHINE=imx8mm-var-dart ./var_make_debian.sh -c kernel
 sudo MACHINE=imx8mm-var-dart ./var_make_debian.sh -c modules
-sudo cp output/Image.gz /media/marcin/rootfs/boot/Image.gz
-sudo cp output/imx8mm-var-dart-dt8mcustomboard-legacy.dtb /media/marcin/rootfs/boot/imx8mm-var-dart-dt8mcustomboard-legacy.dtb
+sudo cp output/Image.gz /media/marcinamc/rootfs/boot/Image.gz
+sudo cp output/imx8mm-var-dart-dt8mcustomboard-legacy.dtb /media/marcinamc/rootfs/boot/imx8mm-var-dart-dt8mcustomboard-legacy.dtb
 sudo sync
-sudo umount /media/marcin/rootfs 
+sudo umount /media/marcinamc/rootfs 
 
 echo "Sdcard ready for device"
